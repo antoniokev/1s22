073c0748ac16c63d3824c3656632d94b1af7d069
antoniokev <144190929+antoniokev@users.noreply.github.com>
2023-09-19 19:30:24 -0600

    Add files via upload

---
+++ p10_1s22.java
@@ -0,0 +1,17 @@
+//programas para manejar condiciones 
+//la palabra si nos ayudara a evaluar expresienes 
+//condicion simple llamada if
+//a>b a<b a!=c
+// 3>4 4<6 5>=7 5<=7
+
+class p10_1s22 {
+	public static void main(String[] args) {
+		int a=4;
+		int b=8;
+		if (false) {
+			System.out.println("por que aparezco...");
+			
+		}
+		System.out.println("que paso...");
+	}
+}
