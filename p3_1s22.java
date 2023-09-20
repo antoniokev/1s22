03fb1fce8281d082e5496ae6a2858c2f32125a0c
antoniokev <144190929+antoniokev@users.noreply.github.com>
2023-09-19 19:28:26 -0600

    Add files via upload

---
+++ p3_1s22.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class p3_1s22 {
+	public static void main(String[] args) {
+		String nombre;
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("ingresa un numbre");
+		nombre = teclado.nextLine();
+		System.out.println("Bienvenido" + nombre + "es la clase de programcion");		
+	}
+}
