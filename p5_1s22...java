8b95d33a8d213a36e5825a97188b6203d9629cdd
antoniokev <144190929+antoniokev@users.noreply.github.com>
2023-09-19 19:28:55 -0600

    Add files via upload

---
+++ p5_1s22...java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class p5_1s22 {
+    public static void main(String[] args) {
+        java.util.Scanner teclado = new Scanner(System.in);
+        String temp;
+        int num1, num2;
+        int resultado;
+
+        System.out.println("ingresa el primer numero; ");
+        temp = teclado.nextLine();
+        num1 = Integer.parseInt(temp);
+        System.out.println("ingresa el segundo numero;");
+        temp = teclado.nextLine();
+        num2 = Integer.parseInt(temp);
+        resultado = num1 - num2;
+        System.out.println("el resultado de la resta es; " + resultado);
+
+        }
+}
